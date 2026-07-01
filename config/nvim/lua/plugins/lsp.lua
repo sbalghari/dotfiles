385cfe1224988f45a19412a68c3cc9d69b85aaa2
sbalghari <sbalghari@proton.me>
2026-07-01 08:11:46 +0500

    fix(nvim): disable pyright in favour of ty

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,9 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        pyright = {
+          enabled = false,
+        },
         ty = {},
         clangd = {},
         ruff = {},
